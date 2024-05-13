a3753899565b37b37b565ae3c20ab7a8af7e052c
vitaminwoo <58829994+vitaminwoo@users.noreply.github.com>
2024-05-13 13:44:43 +0000

    Sec_5 Web MVC Dev. with MemberManegement

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,8 +1,14 @@
 package hello.hellospring.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 
 @Controller
@@ -15,5 +21,28 @@ public class MemberController {
         this.memberService = memberService;
     }
 
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:https://reimagined-chainsaw-r6x4x7rv4v92xvp7-8080.app.github.dev/";
+        //redirect url이 강의와 약간 차이가 있음 주의.
+        //저 r6x 어쩌구 값이 달라질 수도 있는 위험성이...
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
+    }
     
 }
